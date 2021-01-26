36fcb73f3907b86a332b10b064fbe4ba175c49b1
Matteo <metboolean@gmail.com>
2021-01-26 17:59:50 +0100

    ugh

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Aggiungere un’immagine presa anch’essa da un data*/
+
+var app = new Vue({
+  el: '#root',
+
+  data: {
+    title: 'Hello World',
+    img: 'https://lh3.googleusercontent.com/proxy/caSLesSyH03rkoEicJhSk2HpIyejbLfEpzwQ47QxBpl_eMX_7Zn4bn9Xqifouv0vKRnpGUKAyx9powA_ST5mEbfUlayVTqODjXOEJKS-ZiCt_uua0NPViZM89-vxPzI'
+  }
+
+
+})
+Vue.config.devtools = true;
